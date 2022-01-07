8da2168e225a45ab470db99846b419cc3d4545ba
Ayoub Karafi <karafi.work@gmail.com>
2022-01-07 05:26:39 +0100

    done!

--- init/init_table.c
+++ init/init_table.c
@@ -6,7 +6,7 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 13:55:20 by akarafi           #+#    #+#             */
-/*   Updated: 2022/01/07 05:20:58 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/01/07 05:23:54 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	init_table(t_table **table, int ac, char **av, t_list **garbage)
 	(*table) = malloc(sizeof(t_table));
 	add_garbge((*table), garbage);
 	(*table)->nbr_of_philos = ft_atoi(av[1], &error);
-	(*table)->nbr_of_full_philos = 0; 
+	(*table)->nbr_of_full_philos = 0;
 	(*table)->die = ft_atoi(av[2], &error);
 	(*table)->eat = ft_atoi(av[3], &error);
 	(*table)->sleep = ft_atoi(av[4], &error);
