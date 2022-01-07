76988d3e87e27058c73de34a618ded7a93df63ef
Ayoub Karafi <karafi.work@gmail.com>
2022-01-07 07:40:29 +0100

    norm

--- routin/routin.c
+++ routin/routin.c
@@ -6,38 +6,41 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 05:10:46 by akarafi           #+#    #+#             */
-/*   Updated: 2022/01/07 07:31:36 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/01/07 07:40:13 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void	__do_routin(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->fork);
+	pthread_mutex_lock(&philo->table->print);
+	printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
+	pthread_mutex_unlock(&philo->table->print);
+	pthread_mutex_lock(&philo->next->fork);
+	pthread_mutex_lock(&philo->table->print);
+	printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
+	printf("%ld %d is eating\n", get_time2(philo), philo->number);
+	pthread_mutex_unlock(&philo->table->print);
+	philo->last_eat = get_time();
+	usleep(philo->table->eat * 1e3);
+	philo->last_eat = get_time();
+	philo->nbr_of_eats++;
+	pthread_mutex_unlock(&philo->fork);
+	pthread_mutex_unlock(&philo->next->fork);
+	pthread_mutex_lock(&philo->table->print);
+	printf("%ld %d is sleeping\n", get_time2(philo), philo->number);
+	pthread_mutex_unlock(&philo->table->print);
+	usleep(philo->table->sleep * 1e3);
+	pthread_mutex_lock(&philo->table->print);
+	printf("%ld %d is thinking\n", get_time2(philo), philo->number);
+	pthread_mutex_unlock(&philo->table->print);
+}
+
 void	*do_routin(t_philo *philo)
 {
 	while (true)
-	{
-		pthread_mutex_lock(&philo->fork);
-		pthread_mutex_lock(&philo->table->print);
-		printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
-		pthread_mutex_unlock(&philo->table->print);
-		pthread_mutex_lock(&philo->next->fork);
-		pthread_mutex_lock(&philo->table->print);
-		printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
-		printf("%ld %d is eating\n", get_time2(philo), philo->number);
-		pthread_mutex_unlock(&philo->table->print);
-		philo->last_eat = get_time();
-		usleep(philo->table->eat * 1e3);
-		philo->last_eat = get_time();
-		philo->nbr_of_eats++;
-		pthread_mutex_unlock(&philo->fork);
-		pthread_mutex_unlock(&philo->next->fork);
-		pthread_mutex_lock(&philo->table->print);
-		printf("%ld %d is sleeping\n", get_time2(philo), philo->number);
-		pthread_mutex_unlock(&philo->table->print);
-		usleep(philo->table->sleep * 1e3);
-		pthread_mutex_lock(&philo->table->print);
-		printf("%ld %d is thinking\n", get_time2(philo), philo->number);
-		pthread_mutex_unlock(&philo->table->print);
-	}
+		__do_routin(philo);
 	return (NULL);
 }
