029d30473f5a0aa0ac7537536405349d7bbd3edd
Ayoub Karafi <karafi.work@gmail.com>
2022-01-10 00:28:19 +0100

    error because of the funcing slow imac ;)

--- routin/routin.c
+++ routin/routin.c
@@ -12,29 +12,39 @@
 
 #include "philo.h"
 
+static void	__part2(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->table->print);
+	if (philo->number)
+		printf("%ld %d is sleeping\n", get_time2(philo), philo->number);
+	pthread_mutex_unlock(&philo->table->print);
+	usleep(philo->table->sleep * 1e3);
+	pthread_mutex_lock(&philo->table->print);
+	if (philo->number)
+		printf("%ld %d is thinking\n", get_time2(philo), philo->number);
+	pthread_mutex_unlock(&philo->table->print);
+}
+
 static void	__do_routin(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->fork);
 	pthread_mutex_lock(&philo->table->print);
-	printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
+	if (philo->number)
+		printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
 	pthread_mutex_unlock(&philo->table->print);
 	pthread_mutex_lock(&philo->next->fork);
 	pthread_mutex_lock(&philo->table->print);
-	printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
-	printf("%ld %d is eating\n", get_time2(philo), philo->number);
+	if (philo->number)
+		printf("%ld %d has taken a fork\n", get_time2(philo), philo->number);
+	if (philo->number)
+		printf("%ld %d is eating\n", get_time2(philo), philo->number);
 	pthread_mutex_unlock(&philo->table->print);
 	usleep(philo->table->eat * 1e3);
 	philo->last_eat = get_time();
 	philo->nbr_of_eats++;
 	pthread_mutex_unlock(&philo->fork);
 	pthread_mutex_unlock(&philo->next->fork);
-	pthread_mutex_lock(&philo->table->print);
-	printf("%ld %d is sleeping\n", get_time2(philo), philo->number);
-	pthread_mutex_unlock(&philo->table->print);
-	usleep(philo->table->sleep * 1e3);
-	pthread_mutex_lock(&philo->table->print);
-	printf("%ld %d is thinking\n", get_time2(philo), philo->number);
-	pthread_mutex_unlock(&philo->table->print);
+	__part2(philo);
 }
 
 void	*do_routin(t_philo *philo)
