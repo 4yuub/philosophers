5bf61809e5a15c6914cb4aa5b98bb951eb44a10f
Ayoub Karafi <karafi.work@gmail.com>
2022-01-09 21:54:15 +0100

    free memory allocation

--- bonus/wait.c
+++ bonus/wait.c
@@ -6,7 +6,7 @@
 /*   By: akarafi <akarafi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 19:52:28 by akarafi           #+#    #+#             */
-/*   Updated: 2022/01/09 19:56:43 by akarafi          ###   ########.fr       */
+/*   Updated: 2022/01/09 20:00:14 by akarafi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 void	wait_philos(t_pid *pid)
 {
+	t_pid	*tmp;
+
 	waitpid(-1, NULL, 0);
+	tmp = pid;
 	while (pid)
 	{
 		kill(pid->id, SIGQUIT);
 		pid = pid->next;
 	}
+	clear_pids(&tmp);
 }
